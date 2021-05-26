f97ad35d242738d5e8982a968e12c1c4ef88c4c1
Marnus Coetzee <marnus@specno.com>
2021-05-26 12:30:48 +0200

    Updated palindrome

--- Palindrome/index.js
+++ Palindrome/index.js
@@ -1,13 +1,19 @@
 // ABBBA = ABBBA = true
 // ABCD = CDBA = false
 
+// function palindrome(str) {
+//   let reversed = str.split("").reduce((rev, char) => char + rev, "");
+//   if (reversed === str) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
 function palindrome(str) {
-  let reversed = str.split("").reduce((rev, char) => char + rev, "");
-  if (reversed === str) {
-    return true;
-  } else {
-    return false;
-  }
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
