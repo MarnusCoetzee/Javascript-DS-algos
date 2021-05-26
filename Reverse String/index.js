f29b431e63200e98f7afdb1dcbe706610bcb93c5
Marnus Coetzee <marnus@specno.com>
2021-05-26 08:28:56 +0200

    Reverse string algorithm 3 ways

---
+++ Reverse String/index.js
@@ -0,0 +1,36 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+/**
+ * Method 1 - JS
+ */
+// function reverse(str) {
+//   return str.split("").reduce((rev, char) => char + rev, "");
+// }
+
+/**
+ * Method 2 - for of loop
+ */
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+/**
+ * Method 3 -
+ */
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
+
+module.exports = reverse;
